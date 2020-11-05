c698ae7f48b5e4cd5863dcc72bb6640235d56de7
Breno Rodrigues <breno.st.r@hotmail.com>
2020-11-05 11:57:59 -0300

    criando aluquar

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdrGGt8Zu2esCVXtn9xy8SieEfhwnktVw",
+    authDomain: "aluquar-6b65a.firebaseapp.com",
+    databaseURL: "https://aluquar-6b65a.firebaseio.com",
+    projectId: "aluquar-6b65a",
+    storageBucket: "aluquar-6b65a.appspot.com",
+    messagingSenderId: "667227891842",
+    appId: "1:667227891842:web:bbfecf5ca4d6ed0494ec9b"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
